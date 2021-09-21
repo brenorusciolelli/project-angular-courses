48eb31e7c83af610787605f5bea6353212fd0b09
Breno Rusciolelli <breno.brito@acpgroup.com.br>
2021-09-21 20:58:35 -0300

    Create component star

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -13,21 +13,21 @@ export class CourseListComponent implements OnInit {
       {
         id: 1,
         name: "Angular",
-        imageUrl: "",
+        imageUrl: '/assets/images/forms.png',
         price: 99.99,
         code: "XPS-8796",
         duration: 120,
-        rating: 5.4,
+        rating: 4.4,
         releaseDate: 'December, 2 , 2021'
       },
       {
         id: 2,
         name: "Angular: HTTP",
-        imageUrl: "",
+        imageUrl: '/assets/images/http.png',
         price: 45.99,
         code: "LKL-8796",
         duration: 80,
-        rating: 4.4,
+        rating: 3.4,
         releaseDate: 'November, 19 , 2021'
       },
     ];
